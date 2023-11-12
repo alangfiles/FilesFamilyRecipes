92068df5315bd9c5ede0c61999ea85eb233b5ce2
Alan Files <alangfiles@gmail.com>
2023-11-12 18:58:08 -0500

    update mexican stew

--- volume1.js
+++ volume1.js
@@ -472,7 +472,12 @@ var recipes = [
   {
     "title": "Gazpacho #3",
     "type": "SOUPS",
-    "recipe": "5 1/2 c. tomato juice (V-8)\n1 sm. can tomatoes\n2 cans tomato sauce\n1-T. green pepper flakes\n2 T. parsley\n1/2 tsp. garlic powder\n1/2 tsp. paprika\n1/2 tsp. Creole seasonings    Blend together and serve cold.\nMexican Stew—Jane Keahey’s\n\n2 lb. ground beef\n1 large onion, chopped (optional)\nBrown these together, drain, and add the following:\n\t1 pkg. Mild taco seasonings\n\t1 tsp. salt\n\t1/2 tsp. pepper\nMix and add:\n\t2 cans Veg-All (drained)\n\t2 cans plain pinto beans (not drained)\n\t2 can whole kernel corn (not drained)\n\t2 cans sliced new potatoes (drained)\n\t3 sm. cans tomato sauce (or 16 oz.)\n\nSimmer together awhile.  \nEasy dinner to take to someone as dinner served with cornbread, and brownies (maybe salad, too)"
+    "recipe": "5 1/2 c. tomato juice (V-8)\n1 sm. can tomatoes\n2 cans tomato sauce\n1-T. green pepper flakes\n2 T. parsley\n1/2 tsp. garlic powder\n1/2 tsp. paprika\n1/2 tsp. Creole seasonings    Blend together and serve cold.\n"
+  },
+  {
+    "title": "Mexican Stew—Jane Keahey’s",
+    "type": "SOUPS",
+    "2 lb. ground beef\n1 large onion, chopped (optional)\nBrown these together, drain, and add the following:\n\t1 pkg. Mild taco seasonings\n\t1 tsp. salt\n\t1/2 tsp. pepper\nMix and add:\n\t2 cans Veg-All (drained)\n\t2 cans plain pinto beans (not drained)\n\t2 can whole kernel corn (not drained)\n\t2 cans sliced new potatoes (drained)\n\t3 sm. cans tomato sauce (or 16 oz.)\n\nSimmer together awhile.  \nEasy dinner to take to someone as dinner served with cornbread, and brownies (maybe salad, too)",
   },
   {
     "title": "Mushroom Soup—The Breakers’   (Prep time: 45 min.)",
